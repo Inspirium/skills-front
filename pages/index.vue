91d3597cd055778cf671a0918e155379623dbc0c
StjepanD <sdrmic@gmail.com>
2022-03-10 19:13:23 +0100

    update

--- pages/index.vue
+++ pages/index.vue
@@ -26,16 +26,16 @@ const test = computed(() => {
 
 const hover = ref(null)
 
-// onMounted(() => {
-//   gasp.to('#sace', {
-//     duration: 0.5,
-//     opacity: 0,
-//     scale: 0,
-//     y: 200,
-//     ease: 'power1',
-//     stagger: 0.1,
-//   })
-// })
+onMounted(() => {
+  gasp.to('.sace', {
+    duration: 0.5,
+    opacity: 0,
+    scale: 0,
+    y: 200,
+    ease: 'power1',
+    stagger: 0.1,
+  })
+})
 
 function beforeEnter(el) {
   el.style.opacity = 0
@@ -103,19 +103,17 @@ function showslow1() {
       </p>
     </div>
     <div class="flex flex-wrap mt-20">
-      <client-only>
-        <div v-for="(item, index) in sectors" :key="index" :class="hover === index ? 'activecell bounce' : 'bounce-reverse', index === 0 || index === 10 || index === 20 ? 'ml-[6.8rem]' : ''" class="-mr-[3.2rem] w-[270px] h-[240px] relative -mb-[3.2rem] cursor-pointer transition sace" id="sace" @click="router.push(`/sector/${item.id}`)" @mouseover="hover = index" @mouseleave="hover = null">
-          <h3 class="z-10 w-3/5 absolute text-center hex-text-center font-roboto font-medium text-lg text-gray-800 leading-6 -mt-3 transition">{{ item.name }}</h3>
-          <font-awesome-icon  class="absolute hex-icon w-10 h-10 " :color="hover !== index ? `#${item.color}` : '#ffffff'" :icon="item.icon"></font-awesome-icon>
-          <svg class="dropshadow z-20" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-               width="270px" height="240px" viewBox="0 0 141.74 162.98" xml:space="preserve">
-            <polygon fill-rule="evenodd" clip-rule="evenodd" :fill="hover === index ? `#${item.color}` : '#ffffff'" class="text-white transition duration-500" points="141.74,40.62 71.21,0 0,40.87 0,122.36 71.21,162.98
-            141.74,122.12 "/>
-            <path fill-rule="evenodd" clip-rule="evenodd" :fill="`#${item.color}`" d="M141.74,40.62L71.21,0L0,40.87v81.49l71.21,40.62l70.53-40.86
-            V40.62z M71.21,4.44l67.06,38.64v77.46l-67.06,38.87L3.48,120.77V43.31L71.21,4.44z"/>
-          </svg>
-        </div>
-      </client-only>
+      <div v-for="(item, index) in sectors" :key="index" :class="hover === index ? 'activecell bounce' : 'bounce-reverse', index === 0 || index === 10 || index === 20 ? 'ml-[6.8rem]' : ''" class="-mr-[3.2rem] w-[270px] h-[240px] relative -mb-[3.2rem] cursor-pointer transition sace" id="sace" @click="router.push(`/sector/${item.id}`)" @mouseover="hover = index" @mouseleave="hover = null">
+        <h3 class="z-10 w-3/5 absolute text-center hex-text-center font-roboto font-medium text-lg text-gray-800 leading-6 -mt-3 transition">{{ item.name }}</h3>
+        <font-awesome-icon  class="absolute hex-icon w-10 h-10 " :color="hover !== index ? `#${item.color}` : '#ffffff'" :icon="item.icon"></font-awesome-icon>
+        <svg class="dropshadow z-20" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+             width="270px" height="240px" viewBox="0 0 141.74 162.98" xml:space="preserve">
+          <polygon fill-rule="evenodd" clip-rule="evenodd" :fill="hover === index ? `#${item.color}` : '#ffffff'" class="text-white transition duration-500" points="141.74,40.62 71.21,0 0,40.87 0,122.36 71.21,162.98
+          141.74,122.12 "/>
+          <path fill-rule="evenodd" clip-rule="evenodd" :fill="`#${item.color}`" d="M141.74,40.62L71.21,0L0,40.87v81.49l71.21,40.62l70.53-40.86
+          V40.62z M71.21,4.44l67.06,38.64v77.46l-67.06,38.87L3.48,120.77V43.31L71.21,4.44z"/>
+        </svg>
+      </div>
     </div>
   </div>
 
