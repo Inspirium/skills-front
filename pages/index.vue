e3fba401b1c0db56362a9e8fc570c2ee6b084c72
StjepanD <sdrmic@gmail.com>
2022-03-10 11:15:30 +0100

    gasp init

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 
-import {ref} from 'vue'
+import { ref } from 'vue'
+import gsap from 'gsap'
 
 const { $jsonSerializer } = useNuxtApp()
 
@@ -25,12 +26,58 @@ const test = computed(() => {
 
 const hover = ref(null)
 
+// onMounted(() => {
+//   gasp.to('.sace', {
+//     duration: 0.5,
+//     opacity: 0,
+//     scale: 0,
+//     y: 200,
+//     ease: 'power1',
+//     stagger: {
+//       from: 'edges',
+//       each: 0.1
+//     }
+//   })
+// })
+
+// function beforeEnter(el) {
+//   el.style.opacity = 0
+//   el.style.transform = 'scale(0,0)'
+// }
+//
+// function enter(el, done) {
+//   gsap.to(el, {
+//     duration: 10,
+//     opacity: 1,
+//     scale: 1,
+//     ease: 'bounce.out',
+//     onComplete: done
+//   })
+// }
+
+const beforeEnter = (el) => {
+  el.style.opacity = 0
+  el.style.transform = 'scale(0,0)'
+}
+
+const enter = (el) => {
+  gsap.to(el, {
+    duration: 10,
+    opacity: 1,
+    scale: 1,
+    ease: 'bounce.out',
+    onComplete: done
+  })
+}
 
 </script>
 
 <template>
   <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
 <!--    <font-awesome-icon icon="tree"></font-awesome-icon>-->
+    <transition appear @before-enter="beforeEnter" @enter="enter" >
+      <div class="card"></div>
+    </transition>
     <div class="flex justify-between text-white mt-12">
       <div class="bounce-reverse">
         <h1 class="font-semibold sm:text-[5rem] font-dosis">Istraži vještine</h1>
@@ -55,3 +102,14 @@ const hover = ref(null)
   </div>
 
 </template>
+<style scoped>
+.card {
+  display: block;
+  margin: 0 auto 0 auto;
+  height: 6.5em;
+  width: 6.5em;
+  border-radius: 1%;
+  background-color: #16c0b0;
+  box-shadow: 0.08em 0.03em 0.4em #ababab;
+}
+</style>
