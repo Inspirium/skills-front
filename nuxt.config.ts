9dc620698b6c7e433688dc0e2cbd3e16a33ae981
StjepanD <sdrmic@gmail.com>
2022-04-01 17:13:53 +0200

    Favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,12 @@ export default defineNuxtConfig({
   meta: {
     title: 'Katalog digitalnih i zelenih vještina - HZZ.hr',
     description: 'Katalog digitalnih i zelenih vještina',
+    link: [
+      { rel: 'apple-touch-icon', sizes: '180x180', href:'/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes:'16x16', href:'/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/png', sizes:'32x32', href:'/favicon-32x32.png' },
+      { rel: 'manifest', href:'/site.webmanifest' },
+    ],
   },
   buildModules: [
     '@vueuse/nuxt',
