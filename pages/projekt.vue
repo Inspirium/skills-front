c38b7cc6cc76e4989bf10fab6ea3be980fea6d44
Marko Banušić <mbanusic@gmail.com>
2022-04-01 16:41:25 +0200

    cleanup

--- pages/projekt.vue
+++ pages/projekt.vue
@@ -1,115 +1,9 @@
 <script setup lang="ts">
-import gsap from 'gsap'
-
-const { $jsonSerializer } = useNuxtApp()
-
-const router = useRouter()
-const route = useRoute()
-
-const { data: sectors } = await useApiFetch('/api/v1/sectors', {
-  params: {
-    'filter[parent]': false,
-  },
-  parseResponse: txt => $jsonSerializer.deserialize('sectors', JSON.parse(txt)),
-})
-
-const test = computed(() => {
-  return sectors.value.map(item => item.icon)
-})
-
-const hover = ref(null)
-const width = ref(320)
-const loaded = ref(false)
-
-function initAnimation() {
-  console.log('daj nekaj')
-  loaded.value = true
-  window.addEventListener('resize', () => {
-    width.value = window.innerWidth
-  })
-  width.value = window.innerWidth
-  showslow1()
-}
-
-onUpdated(initAnimation)
-onMounted(initAnimation)
-
-onUnmounted(() => {
-  window.removeEventListener('resize', () => {
-    width.value = window.innerWidth
-  })
-})
-
-
-//
-// function beforeEnter(el) {
-//   el.style.opacity = 0
-//   el.style.transform = 'scale(2,2)'
-// }
-//
-// function enter(el, done) {
-//   gsap.to(el, {
-//     duration: 1,
-//     opacity: 1,
-//     scale: 1,
-//     ease: 'bounce.out',
-//     onComplete: done
-//   })
-// }
-
-function showslow1() {
-  gsap.to('.sace', {
-    duration: 0.2,
-    opacity: 1,
-    scale: 1,
-    y: 0,
-    ease: 'power1',
-    stagger: 0.1,
-  })
-}
-
-const lineBreak = computed(() => {
-  let b
-  if (width.value < 450)
-    b = 2
-
-  if (width.value > 450)
-    b = 3
-
-  if (width.value > 580)
-    b = ((width.value - 580) / 234 >> 0) * 2 + 4
-
-  if (b > 10)
-    b = 10
-
-  return b
-})
-
-// const beforeEnter = (el) => {
-//   el.style.opacity = 0
-//   el.style.transform = 'scale(0,0)'
-// }
-//
-// const enter = (el) => {
-//   gsap.to(el, {
-//     duration: 10,
-//     opacity: 1,
-//     scale: 1,
-//     ease: 'bounce.out',
-//     onComplete: done
-//   })
-// }
 
 </script>
 
 <template>
   <div class="max-w-7xl mx-auto px-4 md:px-6 lg:px-8 pb-40">
-    <!--    <font-awesome-icon icon="tree"></font-awesome-icon>-->
-    <!--    <client-only>-->
-    <!--      <transition @before-enter="beforeEnter" @enter="enter" :css="false" appear>-->
-    <!--        <div class="card"></div>-->
-    <!--      </transition>-->
-    <!--    </client-only>-->
     <div class="md:flex justify-center sm:space-x-20 md:mt-12 md:items-center">
       <div class="bounce-reverse justify-center flex flex-col items-center">
         <h1 class="font-bold text-[3rem] leading-[3.5rem] md:my-0 my-6 md:text-[4.5rem] md:leading-[5rem] font-dosis text-grey-700">
@@ -123,10 +17,20 @@ const lineBreak = computed(() => {
       </div>
     </div>
     <div class="max-w-xl text-left mx-auto">
-      <p class=" mt-10">
-        Ministarstvo rada, mirovinskoga sustava, obitelji i socijalne politike (MROSP) tijekom 2022. godine započelo je s provedbom sustava vaučera, čiji je cilj omogućiti veće sudjelovanje nezaposlenih i zaposlenih osoba u cjeloživotnom obrazovanju s naglaskom na stjecanju vještina povezanih sa <NuxtLink to="/" class="font-bold text-lime-500">zelenom</NuxtLink> i <NuxtLink to="/" class="font-bold text-cyan-500">digitalnom</NuxtLink> tranzicijom. Kako bi se detektiralo koji su to obrazovni programi prihvatljivi za dodjelu vaučera, odnosno stjecanje kojih vještina će se financirati kao odgovor na potrebe tržišta rada, u prvom koraku proveden je postupak inicijalnog mapiranja vještina upisanih u Registar HKO sukladno usuglašenoj metodologiji te je kao finalni rezultat izrađen katalog vještina potrebnih za digitalnu i zelenu transformaciju/tranziciju gospodarstva. U drugom koraku Katalog se kontinuirano nadopunjuje novoupisanim zelenim i digitalnim vještinama iz Registra HKO te zelenim i digitalnim vještinama iz vanjskih baza vještina kao što su ESCO, O*NET, CEDEFOP, Canadian Skills and Knowledge Checklist i slično. U trećem koraku u Katalog će biti uključene nove zelene i digitalne vještine dobivene kontinuiranim pregledom oglasa za poslove.
-      <p class="mt-4">Katalog zelenih i digitalnih vještina omogućuje MROSP-u detekciju vještina potrebnih za rad unutar postojećih sektora i podsektora Hrvatskog kvalifikacijskog okvira posebno u kontekstu zelenih i digitalnih tranzicija ključnih za povećanje zapošljivosti, ali i osiguranje postojeće zapošljivosti i prevenciju porasta broja nezaposlenih.</p>
-      <p class="mt-4">Do popisa zelenih i digitalnih  vještine u na <NuxtLink to="/" class="font-bold text-cyan-500">Početnoj stranici Kataloga</NuxtLink> dolazi se na dva načina: (i) kroz odabir željenog sektora i podsektora ili (ii) kroz odabir jedne od ikona Digitalne vještine/Zelene vještine. U prvom slučaju prikazuje se lista zelenih i digitalnih vještina u pojedinom podsektoru, a u drugom lista svih zelenih ili digitalnih vještina trenutno upisanih u Katalog. Odabirom pojedine vještine prikazuju se specifični podaci o vještini kao što su tip, izvor te njena veza s pojedinim standardima zanimanja upisanih u Registar HKO.</p>
+      <p class="mt-10">
+        Ministarstvo rada, mirovinskoga sustava, obitelji i socijalne politike (MROSP) tijekom 2022. godine započelo je s provedbom sustava vaučera, čiji je cilj omogućiti veće sudjelovanje nezaposlenih i zaposlenih osoba u cjeloživotnom obrazovanju s naglaskom na stjecanju vještina povezanih sa <NuxtLink to="/" class="font-bold text-lime-500">
+          zelenom
+        </NuxtLink> i <NuxtLink to="/" class="font-bold text-cyan-500">
+          digitalnom
+        </NuxtLink> tranzicijom. Kako bi se detektiralo koji su to obrazovni programi prihvatljivi za dodjelu vaučera, odnosno stjecanje kojih vještina će se financirati kao odgovor na potrebe tržišta rada, u prvom koraku proveden je postupak inicijalnog mapiranja vještina upisanih u Registar HKO sukladno usuglašenoj metodologiji te je kao finalni rezultat izrađen katalog vještina potrebnih za digitalnu i zelenu transformaciju/tranziciju gospodarstva. U drugom koraku Katalog se kontinuirano nadopunjuje novoupisanim zelenim i digitalnim vještinama iz Registra HKO te zelenim i digitalnim vještinama iz vanjskih baza vještina kao što su ESCO, O*NET, CEDEFOP, Canadian Skills and Knowledge Checklist i slično. U trećem koraku u Katalog će biti uključene nove zelene i digitalne vještine dobivene kontinuiranim pregledom oglasa za poslove.
+      </p>
+      <p class="mt-4">
+        Katalog zelenih i digitalnih vještina omogućuje MROSP-u detekciju vještina potrebnih za rad unutar postojećih sektora i podsektora Hrvatskog kvalifikacijskog okvira posebno u kontekstu zelenih i digitalnih tranzicija ključnih za povećanje zapošljivosti, ali i osiguranje postojeće zapošljivosti i prevenciju porasta broja nezaposlenih.
+      </p>
+      <p class="mt-4">
+        Do popisa zelenih i digitalnih  vještine u na <NuxtLink to="/" class="font-bold text-cyan-500">
+          Početnoj stranici Kataloga
+        </NuxtLink> dolazi se na dva načina: (i) kroz odabir željenog sektora i podsektora ili (ii) kroz odabir jedne od ikona Digitalne vještine/Zelene vještine. U prvom slučaju prikazuje se lista zelenih i digitalnih vještina u pojedinom podsektoru, a u drugom lista svih zelenih ili digitalnih vještina trenutno upisanih u Katalog. Odabirom pojedine vještine prikazuju se specifični podaci o vještini kao što su tip, izvor te njena veza s pojedinim standardima zanimanja upisanih u Registar HKO.
       </p>
     </div>
   </div>
--- pages/subsektor.vue
+++ pages/subsektor.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import {ref} from 'vue'
+import { ref } from 'vue'
 
 const { $jsonSerializer } = useNuxtApp()
 
@@ -16,40 +16,45 @@ function loadIt() {
 }
 loadIt()
 
-const test = computed(() => {
-  return sectors.value.map(item => item.icon)
-})
-
 const hover = ref(null)
-
-
 </script>
 
 <template>
   <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-<!--    <font-awesome-icon icon="tree"></font-awesome-icon>-->
+    <!--    <font-awesome-icon icon="tree"></font-awesome-icon>-->
     <div class="flex justify-between text-white mt-12">
       <div class="">
-        <h1 class="font-semibold sm:text-[6rem] font-dosis">Istraži podgrupe vještine</h1>
-        <h2 class="font-light sm:text-4xl font-dosis -mt-2">Neki zgodan podnaslov</h2>
+        <h1 class="font-semibold sm:text-[6rem] font-dosis">
+          Istraži podgrupe vještine
+        </h1>
+        <h2 class="font-light sm:text-4xl font-dosis -mt-2">
+          Neki zgodan podnaslov
+        </h2>
       </div>
-      <p class="max-w-xl text-right"><span class="text-cyan-400 font-bold text-2xl font-dosis">Poljoprivreda, prehrana i veterina </span> prikazuju grupe sličnih specijalističkih zadataka. Specijalistički zadaci osmišljeni su tako da opisuju svakodnevni rad unutar zanimanja. Ovi zadaci su uglavnom prenosivi – ako možete obaviti jedan zadatak u klasteru, možete obaviti i ostale.
+      <p class="max-w-xl text-right">
+        <span class="text-cyan-400 font-bold text-2xl font-dosis">Poljoprivreda, prehrana i veterina </span> prikazuju grupe sličnih specijalističkih zadataka. Specijalistički zadaci osmišljeni su tako da opisuju svakodnevni rad unutar zanimanja. Ovi zadaci su uglavnom prenosivi – ako možete obaviti jedan zadatak u klasteru, možete obaviti i ostale.
       </p>
     </div>
     <div class="flex flex-wrap mt-20">
-      <div v-for="(item, index) in sectors" :class="hover === index ? 'activecell bounce' : 'bounce-reverse', index === 0 || index === 10 || index === 20 ? 'ml-[6.8rem]' : ''" class="-mr-[3.2rem] w-[270px] h-[240px] relative -mb-[3.2rem] cursor-pointer transition sace" @mouseover="hover = index" @mouseleave="hover = null">
-<!--        <p class="text-">{{ item.color }}</p>-->
-        <h3 class="z-10 w-3/5 absolute text-center hex-text-center font-roboto font-medium text-lg text-gray-800 leading-6 -mt-3 transition">{{ item.name }}</h3>
-        <font-awesome-icon  class="absolute hex-icon w-10 h-10 " :color="hover !== index ? `#${item.color}` : '#ffffff'" :icon="item.icon"></font-awesome-icon>
-        <svg class="dropshadow z-20" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-             width="270px" height="240px" viewBox="0 0 141.74 162.98" xml:space="preserve">
-          <polygon fill-rule="evenodd" clip-rule="evenodd" :fill="hover === index ? `#${item.color}` : '#ffffff'" class="text-white transition duration-500" points="141.74,40.62 71.21,0 0,40.87 0,122.36 71.21,162.98
-          141.74,122.12 "/>
-          <path fill-rule="evenodd" clip-rule="evenodd" :fill="`#${item.color}`" d="M141.74,40.62L71.21,0L0,40.87v81.49l71.21,40.62l70.53-40.86
-          V40.62z M71.21,4.44l67.06,38.64v77.46l-67.06,38.87L3.48,120.77V43.31L71.21,4.44z"/>
+      <div v-for="(item, index) in sectors" :key="index" :class="[hover === index ? 'activecell bounce' : 'bounce-reverse', index === 0 || index === 10 || index === 20 ? 'ml-[6.8rem]' : '']" class="-mr-[3.2rem] w-[270px] h-[240px] relative -mb-[3.2rem] cursor-pointer transition sace" @mouseover="hover = index" @mouseleave="hover = null">
+        <h3 class="z-10 w-3/5 absolute text-center hex-text-center font-roboto font-medium text-lg text-gray-800 leading-6 -mt-3 transition">
+          {{ item.name }}
+        </h3>
+        <font-awesome-icon class="absolute hex-icon w-10 h-10 " :color="hover !== index ? `#${item.color}` : '#ffffff'" :icon="item.icon" />
+        <svg
+          id="Layer_1" class="dropshadow z-20" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+          width="270px" height="240px" viewBox="0 0 141.74 162.98" xml:space="preserve"
+        >
+          <polygon
+            fill-rule="evenodd" clip-rule="evenodd" :fill="hover === index ? `#${item.color}` : '#ffffff'" class="text-white transition duration-500" points="141.74,40.62 71.21,0 0,40.87 0,122.36 71.21,162.98
+          141.74,122.12 "
+          />
+          <path
+            fill-rule="evenodd" clip-rule="evenodd" :fill="`#${item.color}`" d="M141.74,40.62L71.21,0L0,40.87v81.49l71.21,40.62l70.53-40.86
+          V40.62z M71.21,4.44l67.06,38.64v77.46l-67.06,38.87L3.48,120.77V43.31L71.21,4.44z"
+          />
         </svg>
       </div>
     </div>
   </div>
-
 </template>
