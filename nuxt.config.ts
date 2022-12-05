4f548a037765bd1e0dd3b5ea1c80bf181db86590
Marko Banušić <mbanusic@gmail.com>
2022-12-05 11:24:24 +0100

    update nuxt and deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,37 @@
 export default defineNuxtConfig({
   ssr: false,
-  meta: {
-    title: 'Katalog digitalnih i zelenih vještina - HZZ.hr',
-    description: 'Katalog digitalnih i zelenih vještina',
-    link: [
-      { rel: 'apple-touch-icon', sizes: '180x180', href:'/apple-touch-icon.png' },
-      { rel: 'icon', type: 'image/png', sizes:'16x16', href:'/favicon-16x16.png' },
-      { rel: 'icon', type: 'image/png', sizes:'32x32', href:'/favicon-32x32.png' },
-      { rel: 'manifest', href:'/site.webmanifest' },
-    ],
+  app: {
+    head: {
+      title: 'Katalog digitalnih i zelenih vještina - HZZ.hr',
+      meta: [
+        { name: 'description', content: 'Katalog digitalnih i zelenih vještina' },
+      ],
+      link: [
+        {
+          rel: 'apple-touch-icon',
+          sizes: '180x180',
+          href: '/apple-touch-icon.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: '/favicon-16x16.png',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '32x32',
+          href: '/favicon-32x32.png',
+        },
+        {
+          rel: 'manifest',
+          href: '/site.webmanifest',
+        },
+      ],
+    },
   },
-  buildModules: [
+  modules: [
     '@vueuse/nuxt',
     '@nuxtjs/tailwindcss',
   ],
@@ -24,7 +45,10 @@ export default defineNuxtConfig({
       '@fortawesome/vue-fontawesome',
     ],
   },
-  publicRuntimeConfig: {
-    URL: process.env.BASE_URL,
+  runtimeConfig: {
+    public: {
+      URL: process.env.BASE_URL,
+    },
   },
+  devServerHandlers: [],
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,26 +8,26 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'open': 'Open Sans',
-        'dosis': 'Dosis',
-        'roboto': 'Roboto',
+        open: 'Open Sans',
+        dosis: 'Dosis',
+        roboto: 'Roboto',
       },
       gridTemplateColumns: {
-        'container': '1rem 1fr 1rem',
-        'container2': '2rem 1fr 2rem',
+        container: '1rem 1fr 1rem',
+        container2: '2rem 1fr 2rem',
       },
       colors: {
-        'cup': {
-          "50": "#7EC7E1",
-          "100": "#70BCD7",
-          "200": "#62B1CD",
-          "300": "#6FBAD5",
-          "400": "#479BB8",
-          "500": "#0085b2",
-          "600": "#007297",
-          "700": "#1D7A9A",
-          "800": "#106F8F",
-          "900": "#026485",
+        cup: {
+          50: '#7EC7E1',
+          100: '#70BCD7',
+          200: '#62B1CD',
+          300: '#6FBAD5',
+          400: '#479BB8',
+          500: '#0085b2',
+          600: '#007297',
+          700: '#1D7A9A',
+          800: '#106F8F',
+          900: '#026485',
         },
       },
     },
